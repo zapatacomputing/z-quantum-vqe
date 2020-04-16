704c502225c83c43d59ce80320621081f04ca0fd
michalstechly <michal.stechly@gmail.com>
2020-04-16 09:27:51 -0400

    Removed unnecessary comments.

--- src/python/zquantum/vqe/utils_test.py
+++ src/python/zquantum/vqe/utils_test.py
@@ -1,16 +1,6 @@
 import os
 import unittest
 from .utils import build_circuit_template
-# import numpy as np
-# import copy
-# from openfermion.ops import FermionOperator, InteractionOperator, QubitOperator
-# from openfermion.utils import hermitian_conjugated
-# from openfermion.transforms import jordan_wigner, get_interaction_operator
-# from pyquil.wavefunction import Wavefunction
-# from zmachine.algorithms.vqe._api import (build_circuit_template, get_objective_function_from_orbital_frames,
-#                                          get_interaction_rdm_from_expectations, optimize_marginals, create_full_fermion_hamiltonian)
-# from zmachine.core.fermion import get_orbital_frames_decomposition, load_interaction_operator, load_interaction_rdm, transform_fermion_hamiltonian, hf_rdm
-# from zmachine.core.measurement import expectation
 
 class TestGeneralPurpose(unittest.TestCase):
 
