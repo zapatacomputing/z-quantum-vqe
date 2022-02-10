2a113144819c5c4fdad0aa1959216821e089d451
mstechly <michal.stechly@gmail.com>
2022-02-09 20:10:21 -0500

    Updated mypy setup.

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
-import setuptools
+import site
+import sys
 import warnings
+
+import setuptools
 
 try:
     from subtrees.z_quantum_actions.setup_extras import extras
 except ImportError:
     warnings.warn("Unable to import extras")
     extras = {}
+
+# Workaound for https://github.com/pypa/pip/issues/7953
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 
 with open("README.md", "r") as f:
     long_description = f.read()
@@ -31,4 +37,6 @@ setuptools.setup(
     setup_requires=["setuptools_scm~=6.0"],
     install_requires=["z-quantum-core", "openfermion>=1.0.0"],
     extras_require=extras,
+    zip_safe=False,
+    package_data={"ops": ["py.typed"]},
 )
